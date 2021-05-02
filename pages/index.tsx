f10d1d79a724f6811926377b246cbcd409b7dc85
blackSooooo <507pdophs@naver.com>
2021-05-02 18:01:42 +0900

    fix: index파일 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,76 +1,60 @@
 import Layout from "../src/layouts/default";
-import styles from '../src/styles/profile.module.scss';
-import Image from 'next/image';
+import styles from '../src/styles/listup.module.scss'
+import Stylist from '../src/components/stylist';
+import Header from '../src/components/header';
+import Footer from '../src/components/footer';
 import { useState } from 'react';
 
+interface stylist {
+  profileImg: string, 
+  name: string,
+  grade: number,
+  review: number,
+  hired: number,
+  style: string[]
+  introduction: string,
+  photoList: string[],
+};
+
 export default function Home() {
-  const styleList: string[] = ['Trendy', 'Street', 'Nomcore', 'Grunge'];
-  const company: string = "1초 코디에서 근무 중";
-  const career: string = "신입 스타일리스트";
-  const introduction: string = "안녕하세요. 신입 스타일리스트 흑수입니다. 서울에 거주하고 있고, 캐쥬얼한 패션부터 스트릿까지 모두 코디 가능합니다. 트렌디하게 입고 싶기를 원하는 사람은 저에게 연락주세요!!"
-  const imageList: string[] = ["/1.jpeg", "/2.jpeg", "/3.jpeg", "/4.jpeg", "/5.jpeg", "/6.jpeg", "/7.jpeg", "/8.jpeg", "/9.jpeg", "/10.jpeg", "/11.jpeg", "/12.jpeg", "/13.jpeg"]
-  const [selectedImg, setSelectedImg] = useState('');
-  console.log(selectedImg);
-  const ImageClick = (item: string) => {
-    selectedImg != item ? setSelectedImg(item) : setSelectedImg('');
-  }
+  const person: stylist[] = [{
+    profileImg: '/images/blackSooooo/blackSooooo.jpeg',
+    name: '흑수',
+    grade: 4.3,
+    review: 127,
+    hired: 32,
+    style: ['Trendy', 'Street', 'Nomcore'],
+    introduction: '안녕하세요. 신입 스타일리스트 흑수입니다. 서울에 거주하고 있고, 캐쥬얼한 패션부터 스트릿까지 모두 코디 가능합니다. 트렌디하게 입고 싶기를 원하는 사람은 저에게 연락주세요!!',
+    photoList: ["/images/blackSooooo/1.jpeg", "/images/blackSooooo/2.jpeg", "/images/blackSooooo/3.jpeg", "/images/blackSooooo/4.jpeg", "/images/blackSooooo/5.jpeg", "/images/blackSooooo/6.jpeg", "/images/blackSooooo/7.jpeg", "/images/blackSooooo/8.jpeg", "/images/blackSooooo/9.jpeg", "/images/blackSooooo/10.jpeg", "/images/blackSooooo/11.jpeg", "/images/blackSooooo/12.jpeg", "/images/blackSooooo/13.jpeg"]
+  }, {
+    profileImg: '/images/drboy/drboy.jpg',
+    name: '대릭',
+    grade: 4.7,
+    review: 167,
+    hired: 44,
+    style: ['Casual', 'Sporty', 'Natural'],
+    introduction: '안녕하세요. 대릭이에요. 저는 인천에 살고 있고 학교 다니면서 스토어 운영하고 있답니다:) 제 착장들 둘러보시고 마음에 드시면 연락주세요,, 제가 패션왕 만들어드릴게요!! @2c4s',
+    photoList: ['/images/drboy/1.jpeg','/images/drboy/2.jpeg','/images/drboy/3.jpeg','/images/drboy/4.jpeg','/images/drboy/5.jpeg','/images/drboy/6.jpeg','/images/drboy/7.jpeg','/images/drboy/8.jpeg','/images/drboy/9.jpeg', '/images/drboy/10.jpeg','/images/drboy/11.jpeg','/images/drboy/12.jpeg','/images/drboy/13.jpeg','/images/drboy/14.jpeg']
+  },
+  {
+    profileImg: '/images/d.on.gh/d.on.gh.jpg',
+    name: '혁우',
+    grade: 4.4,
+    review: 222,
+    hired: 37,
+    style: ['Monotone', 'Vintage', 'American'],
+    introduction: '안녕하세요 전 혁우에요. 느낌 아니까.',
+    photoList: ['/images/d.on.gh/1.jpeg','/images/d.on.gh/2.jpeg','/images/d.on.gh/3.jpeg','/images/d.on.gh/4.jpeg']
+  }];
+  const [isRanking, setIsRanking] = useState(false);
+
   return (
-    <Layout> 
-      <div className={styles.main}>
+    <Layout>
+      <div className={styles.background}>
         <div className={styles.container}>
-          <div className={styles.profile}>
-            <div className={styles.header}>
-              <Image src="/Back.png" width="8.5" height="17" />
-              <Image src="/menu.png" width="17.5" height="13"/>
-            </div>
-            <div className={styles.name}>
-              <Image src="/profile.jpeg" width="150" height="150" className={styles.image}/>
-              <div style={{marginLeft: 20}}> 
-                Stylist<br />흑수&nbsp;
-                <Image src="/badge.png" width="23" height="23"/>
-              </div>
-            </div>
-            <div className={styles.category}>
-            {styleList.map(item => {
-                return (
-                  <div className={styles.styleBox}>{item}</div>
-                )
-              })}
-            </div>
-          </div>
-          <div className={styles.infoContainer}>
-            <div className={styles.division}>
-              <span style={{fontSize: 25, fontWeight: 'bold', marginRight: 10}}>흑수</span>
-              Stylist
-              <div className={styles.text}>
-                <Image src="/company.png" width="12.02" height="12.07"/>
-                &nbsp;&nbsp;&nbsp;{company}
-              </div>
-              <div className={styles.text}>
-                <Image src="/career.png" width="12" height="12"/>
-                &nbsp;&nbsp;&nbsp;{career}
-              </div>
-            </div>
-            <div className={styles.scrollBox}>
-            <div className={styles.division}>
-              <span style={{fontSize:20, fontWeight: 'bold'}}>자기소개</span><br />
-              <span style={{color: 'rgb(61,61,61)', fontSize: 12}}>{introduction}</span>
-            </div>
-            <span style={{fontSize:20, fontWeight: 'bold'}}>Photo {imageList.length}</span>
-            <div>
-              {imageList.map(item => {
-                return (
-                  <button onClick={() => ImageClick(item)}>
-                  <Image src={item} width="112" height="112" className={styles.clothImage} />
-                  </button>
-                )
-              })}
-            </div>
-            </div>
-          </div>
-        </div>
-        <div className={styles.largeImage}>
-          {selectedImg != '' ? <Image src={selectedImg} width="600" height="600" /> : null}
+          <Header isRanking={isRanking} setIsRanking={setIsRanking}/>
+          <main className={styles.main}>{!isRanking ? person.map(item => <Stylist info={item}/> ) : null}</main>
+          <Footer />
         </div>
       </div>
     </Layout>
