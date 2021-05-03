1da8354b3102c2d159ad6882ffb202b392fb7ace
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-05-04 00:22:02 +0900

    fix: Fix Layout to recieve header and navigation props

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ 
+  children,
+  Header,
+  Navigation, 
+}: { 
+  children: React.ReactNode,
+  Header?: React.ReactNode,
+  Navigation?: React.ReactNode,
+}) {
   return (
     <>
-      <header />
-      <main>{children}</main>
-      <footer />
+      <header>
+        { Header }
+      </header>
+      <main>
+        { children }
+      </main>
+      <nav>
+        { Navigation }
+      </nav>
     </>
   )
+}
+
+Layout.defaultProps = {
+  Header: null,
+  Navigation: null
 }
