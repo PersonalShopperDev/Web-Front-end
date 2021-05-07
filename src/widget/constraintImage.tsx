ed7a16d7d550d67f00b80aad142a726db03315a9
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-05-08 02:29:22 +0900

    fix: Update constraintImage
    now you can give height property

--- src/widget/constraintImage.tsx
+++ src/widget/constraintImage.tsx
@@ -1,22 +1,57 @@
 import Image from 'next/image'
+import { CSSProperties } from 'react'
 import styles from 'sass/widget/constraintImage.module.scss'
 
-export default function ContraintImage({
-  src,
-  width,
-  className,
-}: {
+interface Props {
   src: string
-  width: string | number
-  className? : string
-}) {
+  className?: string
+  objectFit? : NonNullable<JSX.IntrinsicElements['img']['style']>['objectFit']
+}
+
+interface WithWidth extends Props { width: string | number }
+interface WithHeight extends Props { height: string | number }
+
+export default function ContraintImage({
+  src, className, objectFit, ...props
+}: Props & (WithWidth | WithHeight)) {
+  const processProperty = (): {imageStyle : string, style : CSSProperties} => {
+    if ('width' in props && 'height' in props) {
+      const { width, height } = props
+      return {
+        imageStyle: styles.imageDefault,
+        style: {
+          width,
+          height,
+        },
+      }
+    }
+    if ('width' in props) {
+      const { width } = props
+      return {
+        imageStyle: styles.imageByWidth,
+        style: { width },
+      }
+    }
+    if ('height' in props) {
+      const { height } = props
+      return {
+        imageStyle: styles.imageByHeight,
+        style: { height },
+      }
+    }
+    return null
+  }
+
+  const { imageStyle, style } = processProperty()
+
   return (
-    <figure className={`${styles.container} ${className}`} style={{ width }}>
-      <Image src={src} layout="fill" objectFit="cover" />
-    </figure>
+    <div className={`${imageStyle} ${className}`} style={style}>
+      <Image src={src} layout="fill" objectFit={objectFit} />
+    </div>
   )
 }
 
 ContraintImage.defaultProps = {
+  objectFit: 'cover',
   className: null,
 }
