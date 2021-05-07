881ec2bf1392e32ff865743881ce4a260015e738
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-05-07 16:48:32 +0900

    chore: Add widget alias

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    env: {
-        NAVER_CLIENT_ID: '7BZSX67h7sbrKCQSOsKH',
-        NAVER_SECRET_ID: 'Ttz6voUom1'
-    }
+  env: {
+    NAVER_CLIENT_ID: '7BZSX67h7sbrKCQSOsKH',
+    NAVER_SECRET_ID: 'Ttz6voUom1',
+  },
 }
