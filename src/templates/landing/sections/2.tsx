e56553d7f4ce5d130a6893da79af9140488eeb0f
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-05-08 02:33:03 +0900

    feat: Add two sections of content

--- src/templates/landing/sections/2.tsx
+++ src/templates/landing/sections/2.tsx
@@ -1,9 +1,52 @@
+import { useScroll } from 'providers/scrollProvider'
+import ScrollTrigger from 'providers/scrollProvider/scrollTrigger'
+import { useEffect, useRef } from 'react'
 import styles from 'sass/templates/landing.module.scss'
 
 export default function Section2() {
+  let key = 0
+  const { getScrollY } = useScroll()
+
+  const carouselRef = useRef<HTMLDivElement>()
+
+  const rotateCarousel = () => {
+    requestAnimationFrame(rotateCarousel)
+    carouselRef.current.style.transform = `translate3d(100px, 200px, 0) rotateX(40deg) rotateY(${(getScrollY() * 0.5) % 360}deg)`
+  }
+
+  useEffect(() => {
+    requestAnimationFrame(rotateCarousel)
+  }, [])
+
   return (
     <section className={styles.section2}>
-      <p>Increase you sight</p>
+      <video className={styles.video} autoPlay loop muted>
+        <source src="/videos/space.mp4" type="video/mp4" />
+        <track kind="captions" />
+      </video>
+      <div className={styles.stage}>
+        <div className={styles.carousel} ref={carouselRef}>
+          {[...Array(6)].map(() => {
+            key += 1
+            return (
+              <div key={key} className={styles.panel}>
+                <div className={styles.content} />
+              </div>
+            )
+          })}
+        </div>
+      </div>
+      <div className={styles.messageWrapper}>
+        <ScrollTrigger
+          transform={{
+            from: 'transform: translateY(100%);',
+            to: 'transform: traslateY(0%);',
+          }}
+          transition="transform 1s"
+        >
+          <p className={styles.message}>Resting my wings for a time</p>
+        </ScrollTrigger>
+      </div>
     </section>
   )
 }
