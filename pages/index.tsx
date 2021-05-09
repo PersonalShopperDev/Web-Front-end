8dc51f215136251cf3b26252c47561530f6c533a
blackSooooo <507pdophs@naver.com>
2021-05-09 17:39:39 +0900

    fix: check navigation, modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
-import Layout from "../src/layouts/default";
-import Navigation from '../src/components/navigation';
-import StylistListView from "../src/templates/stylistListView";
+import { useState } from 'react';
+import Layout from "src/layouts/default";
+import Navigation from 'src/components/navigation';
+import Modal from 'src/components/modal';
+import styles from 'sass/modal.module.scss';
 
 export default function Home() {
+  const [showModal, setShowModal] = useState(false);
   return (
     <Layout
-      Main={
-        <StylistListView />
-      }
-      Navigation={
+      Header={
         <Navigation />
+      }
+      children={
+        <div className={styles.background}>
+          <button onClick={() => setShowModal(!showModal)}>
+            Click here
+          </button>
+          <Modal showModal={showModal} setShowModal={setShowModal}/>
+        </div>
       }
     />
   )
