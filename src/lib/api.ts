bef0d2108c62ec015104672a4d0be49d58ecff80
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-05-10 03:15:13 +0900

    feat: Add default post method

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,3 +1,14 @@
-const getApiUrl = (route : string) : string => `http://devapi.ap-northeast-2.elasticbeanstalk.com/v1/${route}`
+export const getApiUrl = (route: string): string => `http://devapi.ap-northeast-2.elasticbeanstalk.com/v1/${route}`
 
-export default getApiUrl
+export default (() => {
+  const post = async (url: string, payload: any): Promise<Response> => fetch(url, {
+    body: JSON.stringify(payload),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+  })
+  return {
+    post,
+  }
+})()
