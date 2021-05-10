b7d914969b358d152f1e04db023c7ffa903babc3
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-05-10 14:47:34 +0900

    style: apply alias

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app'
-import AuthProvider from '../src/providers/authProvider'
+import AuthProvider from 'providers/authProvider'
 
 const modalContainerID = 'modalContainer'
 
