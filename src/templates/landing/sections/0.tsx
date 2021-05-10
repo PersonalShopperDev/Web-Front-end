581737d7e1bdb793cbefc2a83388d16d59036b9c
blackSooooo <507pdophs@naver.com>
2021-05-11 01:01:44 +0900

    feat: Add title, content Prpos

--- src/templates/landing/sections/0.tsx
+++ src/templates/landing/sections/0.tsx
@@ -4,7 +4,8 @@ import styles from 'sass/templates/landing.module.scss'
 
 export default function Section0() {
   const [active, setActive] = useState(false)
-
+  const title: string = "Personal Shopper"
+  const content: string = "화이팅"
   return (
     <section className={styles.section0}>
       <video className={styles.video} autoPlay loop muted>
@@ -16,7 +17,7 @@ export default function Section0() {
         <p className={styles.description}>{'Per me si va ne la citta dolente, per me si va ne l\'etterno dolore, per me si va tra la perduta gente. Giustizia mosse il mio alto fattore; fecemi la divina podestate, la somma sapienza e \'l primo amore. Dinanzi a me non fuor cose create se non etterne, e io etterno duro. Lasciate ogni speranza, voi ch\'intrate.'}</p>
         <button className={styles.learnMore} type="button" onClick={() => setActive(true)}>Learn more</button>
       </div>
-      <Modal showModal={active} setShowModal={setActive} />
+      <Modal showModal={active} setShowModal={setActive} title={title} content={content}/>
     </section>
   )
 }
