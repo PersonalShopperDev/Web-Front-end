bda8dcbd909e07be108681c86e46478e845b0b53
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-05-10 14:40:29 +0900

    fix: Extends default post method

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,13 +1,19 @@
 export const getApiUrl = (route: string): string => `http://devapi.ap-northeast-2.elasticbeanstalk.com/v1/${route}`
 
 export default (() => {
-  const post = async (url: string, payload: any): Promise<Response> => fetch(url, {
-    body: JSON.stringify(payload),
-    headers: {
+  const post = async (url: string, payload: any, options? : RequestInit): Promise<Response> => {
+    const body = options?.body || JSON.stringify(payload)
+    const headers = options?.headers || {
       'Content-Type': 'application/json',
-    },
-    method: 'POST',
-  })
+    }
+    const method = 'POST'
+    return fetch(url, {
+      ...options,
+      body,
+      headers,
+      method,
+    })
+  }
   return {
     post,
   }
