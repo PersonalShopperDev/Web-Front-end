5156c52bee7c8399f364b78c3910e39b298ae764
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-05-12 20:48:06 +0900

    Add viewport meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 import AuthProvider from 'providers/authProvider'
 import 'sass/global.scss'
 
@@ -6,10 +7,15 @@ const modalContainerID = 'modalContainer'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-      <div id={modalContainerID} />
-    </AuthProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <AuthProvider>
+        <Component {...pageProps} />
+        <div id={modalContainerID} />
+      </AuthProvider>
+    </>
   )
 }
 
