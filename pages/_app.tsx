1187aaa7bca46679261d180805f183c9675954d6
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-05-27 12:19:01 +0900

    feat: Add title meta head"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,13 @@ import Head from 'next/head'
 import AuthProvider from 'providers/authProvider'
 import 'sass/global.scss'
 
+const title = '퍼스널 쇼퍼'
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <title>{title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <AuthProvider>
