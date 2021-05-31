cb900b31ffc69c22fa2e599ab2c2c6c569b1defc
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-05-31 17:35:44 +0900

    feat: Add section header

---
+++ src/widget/section-header.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import React from 'react'
+import styles from 'sass/widget/section-header.module.scss'
+import Icon from './icon'
+
+export default function SectionHeader({
+  title,
+  moreHref,
+} : {
+  title : React.ReactNode,
+  moreHref : string,
+}) {
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.title}>{title}</h2>
+      <Link href="/">
+        <a className={styles.more} href={moreHref}>
+          more
+          <Icon size={6} src="back-small.png" />
+        </a>
+      </Link>
+    </section>
+  )
+}
