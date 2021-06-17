f97c024f662e6d9f4570ef391f37265e7c0fdc37
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-06-17 23:38:55 +0900

    fix: Let client redirect depending authentication

--- pages/login/[provider]/callback.tsx
+++ pages/login/[provider]/callback.tsx
@@ -3,6 +3,9 @@ import { useEffect } from 'react'
 import Layout from 'layouts/default'
 import { useAuth } from 'providers/authProvider'
 import { processToken as processNaverToken } from 'providers/authProvider/naverAuthProvider'
+import { GetServerSideProps } from 'next'
+import getServerSideAuth from 'lib/server/auth'
+import providers from 'lib/config/provider'
 
 export default function Page() {
   const router = useRouter()
@@ -22,7 +25,11 @@ export default function Page() {
     if (!provider) {
       return
     }
-    authenticate(provider as string, getToken())
+    const token = getToken()
+    if (!token) {
+      router.push('/')
+    }
+    authenticate(provider as string, token)
   }, [router])
 
   return (
@@ -30,4 +37,26 @@ export default function Page() {
       <p>processing ...</p>
     </Layout>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { provider } = context.params
+  if (!providers.includes(provider as string)) {
+    return {
+      notFound: true,
+    }
+  }
+
+  const { authenticated } = await getServerSideAuth(context)
+  if (authenticated) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+  return {
+    props: {},
+  }
 }
