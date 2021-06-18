d2f23b25b73495b8cfa7d6cc4536adf56432dbff
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-06-18 21:17:00 +0900

    feat: Add temporary signout

--- pages/login/error.tsx
+++ pages/login/error.tsx
@@ -1,6 +1,16 @@
 import Layout from 'layouts/default'
+import { useRouter } from 'next/dist/client/router'
+import { useAuth } from 'providers/authProvider'
+import { useEffect } from 'react'
 
 export default function Page() {
+  const router = useRouter()
+  const { signOut } = useAuth()
+
+  useEffect(() => {
+    signOut()
+    router.push('/')
+  }, [])
   return (
     <Layout>
       <p>login failed</p>
