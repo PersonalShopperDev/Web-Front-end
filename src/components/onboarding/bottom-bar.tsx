5768de4c0d37cc7c1a3c9a74626a7c9af1e3facb
blackSooooo <507pdophs@naver.com>
2021-06-18 12:55:29 +0900

    fix: divide step 3-1, 3-2

--- src/components/onboarding/bottom-bar.tsx
+++ src/components/onboarding/bottom-bar.tsx
@@ -4,17 +4,29 @@ import Image from 'next/image'
 
 export default function bottomBar({
   index,
+  nextStep,
   setIndex,
+  setNextStep,
 }: {
     index: Number,
+    nextStep: boolean,
     setIndex: (value: number) => void;
+    setNextStep: (value: boolean) => void;
 }) {
   const onPrevButtonClick = () => {
-    if (index > 1) setIndex(+index - 1)
+    if (index > 1 && !nextStep) {
+      setIndex(+index - 1)
+    } else if (nextStep) {
+      setNextStep(!nextStep)
+    }
   }
   const onNextButtonClick = () => {
     if (index < 6) {
-      setIndex(+index + 1)
+      if (index === 3 && !nextStep) {
+        setNextStep(true)
+      } else {
+        setIndex(+index + 1)
+      }
     } else if (index === 6) {
       console.log('welcome personal Shopper')
     }
