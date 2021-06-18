2acb1920b0fac340963982eb7d8e48bec3472d93
blackSooooo <507pdophs@naver.com>
2021-06-18 12:57:58 +0900

    feat: Add dummy step5, step6

---
+++ src/templates/onboarding/steps/step5.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from 'sass/templates/onboarding/step.module.scss'
+
+export default function Step5() {
+  return (
+    <section>
+      <h1 className={styles.title}>STEP 5</h1>
+      <h2 className={styles.content}>원하는 상품의 가격대를 선택해주세요</h2>
+    </section>
+  )
+}
---
+++ src/templates/onboarding/steps/step6.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from 'sass/templates/onboarding/step.module.scss'
+
+export default function Step6() {
+  return (
+    <section>
+      <h1 className={styles.title}>STEP 6</h1>
+      <h2 className={styles.content}>다음 사진 중 마음에 드는 스타일을 골라주세요</h2>
+    </section>
+  )
+}
