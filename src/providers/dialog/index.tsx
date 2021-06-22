daecd8ae39c9f0205a349923d65c9a7f0ffbb0da
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-06-22 11:18:28 +0900

    style: Correct property name of Modal

--- src/providers/dialog/index.tsx
+++ src/providers/dialog/index.tsx
@@ -125,7 +125,7 @@ export default function DialogProvider({
           className={className}
           key={id}
           immediate
-          onOff={getExecutions().cancle}
+          onClose={getExecutions().cancle}
         >
           {inner}
         </Modal>
