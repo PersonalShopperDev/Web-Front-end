5e245d8779a48f8eb37e331ab62dc75ca718e0a3
blackSooooo <507pdophs@naver.com>
2021-06-22 23:26:22 +0900

    feat: add last step

--- src/components/onboarding/bottom-bar.tsx
+++ src/components/onboarding/bottom-bar.tsx
@@ -3,28 +3,44 @@ import styles from 'sass/components/bottom-bar.module.scss'
 import Image from 'next/image'
 
 export default function bottomBar({
+  stepIndex,
   onPrevButtonClick,
   onNextButtonClick,
 }: {
+  stepIndex: number
   onPrevButtonClick: () => void
   onNextButtonClick: () => void
 }) {
   return (
     <section className={styles.container}>
-      <button
-        type="button"
-        onClick={() => onPrevButtonClick()}
-        className={styles.prevButton}
-      >
-        <Image src="/icons/prevButton.png" width="6" height="13" />
-      </button>
-      <button
-        type="button"
-        onClick={() => onNextButtonClick()}
-        className={styles.nextButton}
-      >
-        <span className={styles.nextText}>다음</span>
-      </button>
+      {stepIndex === 6
+        ? (
+          <button
+            type="button"
+            onClick={() => onNextButtonClick()}
+            className={styles.completeButton}
+          >
+            <span className={styles.nextText}>확인하기</span>
+          </button>
+        )
+        : (
+          <>
+            <button
+              type="button"
+              onClick={() => onPrevButtonClick()}
+              className={styles.prevButton}
+            >
+              <Image src="/icons/prevButton.png" width="6" height="13" />
+            </button>
+            <button
+              type="button"
+              onClick={() => onNextButtonClick()}
+              className={styles.nextButton}
+            >
+              <span className={styles.nextText}>다음</span>
+            </button>
+          </>
+        )}
     </section>
   )
 }
