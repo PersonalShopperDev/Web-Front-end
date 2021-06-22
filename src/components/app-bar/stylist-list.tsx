4fe04ff4ab26129a168858b8512fb7fff1211386
blackSooooo <507pdophs@naver.com>
2021-06-22 23:25:49 +0900

    feat: app-bar change

--- src/components/app-bar/stylist-list.tsx
+++ src/components/app-bar/stylist-list.tsx
@@ -1,10 +1,15 @@
-import Icon from 'src/widget/icon'
+import Icon from 'widgets/icon'
+import { useRouter } from 'next/router'
 import AppBar from '.'
 
 export default function StylistListAppBar() {
+  const router = useRouter()
   const onClickSearch = () => (
-    console.log('move to stylist-search')
+    router.push('/stylist/search')
   )
+  const onClickFilter = () => {
+    console.log('onclick filter')
+  }
   return (
     <AppBar
       title="스타일리스트"
@@ -15,6 +20,12 @@ export default function StylistListAppBar() {
           src="search.png"
           size={24}
           onClick={onClickSearch}
+        />,
+        <Icon
+          key="icon"
+          src="filter.png"
+          size={23}
+          onClick={onClickFilter}
         />,
       ]}
     />
