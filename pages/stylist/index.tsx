2353f8a28957b6dca23865c08e7610b699b795a1
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-07-08 14:44:47 +0900

    fix: local provider

--- pages/stylist/index.tsx
+++ pages/stylist/index.tsx
@@ -1,15 +1,18 @@
 import Layout from 'layouts/default'
 import List from 'templates/stylist/list'
-//import Navigation from 'src/components/navigation'
+// import Navigation from 'src/components/navigation'
 import StylistListAppBar from 'src/components/app-bar/stylist-list'
+import IntegratedInfinityScrollProvider from 'providers/infinity-scroll/integrated'
 
 export default function Page() {
   return (
     <Layout
       header={<StylistListAppBar />}
-      //bottom={<Navigation />}
+      // bottom={<Navigation />}
     >
-      <List />
+      <IntegratedInfinityScrollProvider>
+        <List />
+      </IntegratedInfinityScrollProvider>
     </Layout>
   )
 }
--- pages/stylist/profile.tsx
+++ pages/stylist/profile.tsx
@@ -2,12 +2,15 @@ import Layout from 'layouts/default'
 import Profile from 'templates/stylist/profile'
 import StylistProfileAppBar from 'src/components/app-bar/stylist-profile'
 import { withRouter } from 'next/router'
+import IntegratedInfinityScrollProvider from 'providers/infinity-scroll/integrated'
 
 const Page = ({ router: { query } }) => (
   <Layout
     header={<StylistProfileAppBar />}
   >
-    <Profile id={query.id} />
+    <IntegratedInfinityScrollProvider>
+      <Profile id={query.id} />
+    </IntegratedInfinityScrollProvider>
   </Layout>
 )
 export default withRouter(Page)
