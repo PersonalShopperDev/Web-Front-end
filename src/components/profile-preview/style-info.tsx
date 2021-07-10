b2b040283e0062fa51f8f0da8981107ccfdd1b57
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-07-10 21:37:35 +0900

    fix: fix bodytype, public height, weight

--- src/components/profile-preview/style-info.tsx
+++ src/components/profile-preview/style-info.tsx
@@ -2,34 +2,37 @@ import Section from 'components/profile/section'
 import styles from 'sass/components/profile-preview/style-info.module.scss'
 
 interface Data {
+  isPublic: boolean,
   height: number,
   weight: number,
-  body: number,
+  value: string,
 }
 
 export default function StyleInfo({ data } : { data : Data }) {
-  const { height, weight, body } = data
+  const {
+    isPublic, height, weight, value,
+  } = data
   return (
     <Section
       head="스타일 정보"
     >
       <div className={styles.container}>
-        {height && (
+        {isPublic && height && (
           <div className={styles.row}>
             <span className={styles.property}>키</span>
             <span className={styles.value}>{`${height}cm`}</span>
           </div>
         )}
-        {weight && (
+        {isPublic && weight && (
           <div className={styles.row}>
             <span className={styles.property}>몸무게</span>
             <span className={styles.value}>{`${weight}kg`}</span>
           </div>
         )}
-        {body && (
+        {value && (
           <div className={styles.row}>
             <span className={styles.property}>체형</span>
-            <span className={styles.value}>{`${body}`}</span>
+            <span className={styles.value}>{value}</span>
           </div>
         )}
       </div>
