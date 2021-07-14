b556c4ad6bcf6e8ce5557b2906f1bc03cd9ebad2
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-07-14 19:04:58 +0900

    feat: Add chat-banner

---
+++ pages/chat/index.tsx
@@ -0,0 +1,56 @@
+import Layout from 'layouts/default'
+import { BannerData } from 'components/banner'
+import { BeforeAfterData } from 'components/before-after'
+import { SupplierData, DemanderData } from 'components/stylist-grid-view'
+import { GetServerSideProps } from 'next'
+import parseJwt from 'lib/util/jwt'
+import { ACCESS_TOKEN, UserType } from 'providers/auth'
+import ChatBanner from 'components/chat-banner'
+
+interface Props {
+  data: Data
+}
+
+interface Data {
+  userType: UserType
+  banners: BannerData[]
+  suppliers: SupplierData[]
+  demanders: DemanderData[]
+  reviews: BeforeAfterData[]
+}
+
+export default function Page({ data } : Props) {
+  return (
+    <Layout>
+      <ChatBanner src="none" />
+    </Layout>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const token = context.req.cookies[ACCESS_TOKEN]
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    }
+  }
+
+  const { userType } = parseJwt(token)
+
+  if (userType === 'N') {
+    return {
+      redirect: {
+        destination: '/onboarding',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {},
+  }
+}
