02cfa69aa221f32d341a27e00b39b71da7dfaec0
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-07-14 21:05:00 +0900

    fix: layout scroll fix

--- src/components/onboarding/supply-step5.tsx
+++ src/components/onboarding/supply-step5.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import styles from 'sass/components/supply-step5.module.scss'
+import Icon from 'widgets/icon'
 
 export default function SupplyStep3() {
   return (
-    <section>
-      <img src="/icons/welcomeSupply.png" alt="selectedIcons" className={styles.image} />
-      <div className={styles.container}>
-        <span>스타일리스트로 오신걸 환영합니다~</span>
-        <span>프로필을 완성하여 사용자와 매칭해보세요</span>
+    <section className={styles.section}>
+      <div className={styles.box}>
+        <Icon src="welcomeSupply.png" size={490} className={styles.image} />
+        <div className={styles.text}>
+          스타일리스트로 오신걸 환영합니다~
+          <br />
+          프로필을 완성하여 사용자와 매칭해보세요
+        </div>
       </div>
     </section>
   )
