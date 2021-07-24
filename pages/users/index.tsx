5bfabc8feffa5c3b50a99379c00d27d4b443b5d1
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-07-24 15:30:30 +0900

    feat: Add users list, profile, search pages

---
+++ pages/users/index.tsx
@@ -0,0 +1,43 @@
+import Layout from 'layouts/default'
+import List from 'templates/user-list/list'
+// import Navigation from 'src/components/navigation'
+import UserListAppBar from 'components/app-bar/user-list'
+import InfinityScrollProvider from 'providers/infinity-scroll'
+import UserListProvider from 'providers/user-list'
+import { GetServerSideProps } from 'next'
+import { ACCESS_TOKEN } from 'providers/auth'
+import parseJwt from 'lib/util/jwt'
+
+export default function Page({ userType }) {
+  return (
+    <UserListProvider>
+      <Layout
+        header={<UserListAppBar userType={userType} />}
+      >
+        <InfinityScrollProvider>
+          <List userType={userType} />
+        </InfinityScrollProvider>
+      </Layout>
+    </UserListProvider>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const token = context.req.cookies[ACCESS_TOKEN]
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+  const { userType } = parseJwt(token)
+
+  return {
+    props: {
+      userType,
+    },
+  }
+}
