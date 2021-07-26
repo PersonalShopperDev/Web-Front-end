dc5d9026f930f5e26a93942df0eaf922b1832901
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-07-26 19:10:13 +0900

    feat: Add unread chat indicator

---
+++ src/components/navigation/menu.tsx
@@ -0,0 +1,47 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import styles from 'sass/components/navigation.module.scss'
+
+export default function Menu({
+  href,
+  title,
+  selectedPath,
+  notSelectedPath,
+  className,
+}: {
+  href: string
+  title: string
+  selectedPath: string
+  notSelectedPath: string
+  className: string
+}) {
+  const router = useRouter()
+
+  const isCurrentPath = () => {
+    const { asPath } = router
+    if (href === '/') {
+      return asPath === href || asPath.includes('users')
+    }
+    return asPath.includes(href)
+  }
+
+  return (
+    <Link href={href === 'none' ? '/' : href}>
+      <a className={className} href={href === 'none' ? '/' : href}>
+        <img
+          src={isCurrentPath() ? selectedPath : notSelectedPath}
+          alt={title}
+          width={18}
+          height={18}
+        />
+        <span
+          className={
+            isCurrentPath() ? styles.selectedText : styles.notSelectedText
+          }
+        >
+          {title}
+        </span>
+      </a>
+    </Link>
+  )
+}
