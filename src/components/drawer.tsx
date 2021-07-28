8fcf84d5ddadd0d0e14a6899bfc6e004a9d662c4
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-07-28 14:54:46 +0900

    fix: add /history in drawer

--- src/components/drawer.tsx
+++ src/components/drawer.tsx
@@ -72,10 +72,10 @@ const categories = [
   //   title: '내 스크랩 보기',
   //   link: '/scrap',
   // },
-  // {
-  //   title: '결제내역',
-  //   link: '/account',
-  // },
+  {
+    title: '결제내역',
+    link: '/history',
+  },
   {
     title: '서비스 이용약관',
     link: '/term/service',
