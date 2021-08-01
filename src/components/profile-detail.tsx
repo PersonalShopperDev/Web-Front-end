4313f59aa8e4113e2387047b9ed278f30af408af
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-08-01 22:28:40 +0900

    fix: layout detail change

--- src/components/profile-detail.tsx
+++ src/components/profile-detail.tsx
@@ -70,7 +70,9 @@ export default function ProfileDetail({
         <div className={styles.titleText}>대표 코디</div>
         <div className={styles.coordContainer}>
           {coord.map(({ img, id }) => (
-            <img src={img} width="164" height="171" className={styles.clothImage} alt="대표 코디" key={id} />
+            <div className={styles.clothImage}>
+              <img src={img} width="164" height="171" alt="대표 코디" key={id} />
+            </div>
           ))}
         </div>
       </div>
