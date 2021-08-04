a96779bbdd254192a0ac549b3129030443f634e1
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-08-04 23:45:50 +0900

    feat: Add chat push

---
+++ src/components/chat/push-link.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import Icon from 'widgets/icon'
+import styles from 'sass/components/chat/push-link.module.scss'
+
+export default function PushLink() {
+  return (
+    <Link href="/chat/push">
+      <a className={styles.container} href="/chat/push">
+        <span className={styles.message}>원활한 서비스를 위한 채팅 알람 받기</span>
+        <Icon src="right-arrow-white.png" size={14} />
+      </a>
+    </Link>
+  )
+}
