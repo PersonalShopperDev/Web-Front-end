861fa5bf901e579cb38600df53edc3f4a712bc83
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-08-07 20:06:53 +0900

    hotfix: Fix script tag for analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,15 +9,18 @@ export default class RootDocument extends Document {
     return (
       <Html>
         <Head>
-          <script>
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-M5G3WBR');`}
-          </script>
-          <script>
-            {`!function(f,b,e,v,n,t,s)
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M5G3WBR');`,
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `!function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
             if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -26,8 +29,9 @@ export default class RootDocument extends Document {
             s.parentNode.insertBefore(t,s)}(window,document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '570808527254325'); 
-            fbq('track', 'PageView');`}
-          </script>
+            fbq('track', 'PageView');`,
+            }}
+          />
           <meta charSet="UTF-8" />
           <link rel="icon" href="/favicon.ico" />
           <script
@@ -40,24 +44,25 @@ export default class RootDocument extends Document {
             src="https://developers.kakao.com/sdk/js/kakao.js"
             charSet="utf-8"
           />
-          <noscript dangerouslySetInnerHTML={{
-            __html: `<img
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<img
             height="1"
             width="1"
             src="https://www.facebook.com/tr?id=570808527254325&ev=PageView&noscript=1"
           />`,
-          }}
+            }}
           />
         </Head>
         <body>
-          <noscript dangerouslySetInnerHTML={{
-            __html: `<iframe
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<noscript><iframe
               src="https://www.googletagmanager.com/ns.html?id=GTM-M5G3WBR"
               height="0"
               width="0"
-              style="display:none;visibility:hidden"
-            ></iframe>`,
-          }}
+              style="display:none;visibility:hidden"></iframe></noscript>`,
+            }}
           />
           <Main />
           <NextScript />
