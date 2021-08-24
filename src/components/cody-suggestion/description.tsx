08938e4f972445185fb117f17fe15ed4958375d5
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-08-25 00:40:53 +0900

    feat: Add new cody-suggesstion components

--- src/components/cody-suggestion/description.tsx
+++ src/components/cody-suggestion/description.tsx
@@ -1,43 +1,39 @@
 /* eslint-disable no-param-reassign */
-import React, {
-  ChangeEvent, useRef, useEffect, MutableRefObject,
-} from 'react'
+import React, { ChangeEvent, useRef, useEffect } from 'react'
 import styles from 'sass/components/description.module.scss'
-import { ProductDescription } from 'templates/cody-suggestion/index'
+import { useCodySuggestion } from 'providers/cody-suggestion'
 
-export default function Description({
-  descriptionRef,
-  description,
-}: {
-  descriptionRef: MutableRefObject<ProductDescription>
-  description: ProductDescription
-}) {
+export default function Description() {
+  const { descriptionRef } = useCodySuggestion()
   const titleRef = useRef<HTMLInputElement>()
   const contentRef = useRef<HTMLTextAreaElement>()
+
   const update = (
     e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>,
     key: string,
   ) => {
     descriptionRef.current[key] = e.target.value
   }
+
   const onChangeInput = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
     update(e, e.target.id)
   }
 
   useEffect(() => {
-    titleRef.current.value = description.title
-    contentRef.current.value = description.content
-  }, [description])
+    titleRef.current.value = descriptionRef.current.title
+    contentRef.current.value = descriptionRef.current.content
+  }, [])
+
   return (
     <>
       <div className={styles.title}>
-        <input type="text" ref={titleRef} placeholder="제목" id="title" onChange={onChangeInput} />
+        <input type="text" ref={titleRef} placeholder="코디제목" id="title" onChange={onChangeInput} />
       </div>
       <div className={styles.description}>
         <textarea
           ref={contentRef}
           placeholder="이 코디에 대한 설명을 써주세요"
-          rows={10}
+          rows={20}
           id="content"
           onChange={onChangeInput}
         />
