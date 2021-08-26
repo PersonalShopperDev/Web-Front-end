44b050869e5e6efc70cc4f12d22bc5eb15340504
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-08-26 12:54:41 +0900

    fix: complete-suggestion refactor

--- pages/suggestion/[id].tsx
+++ pages/suggestion/[id].tsx
@@ -2,20 +2,21 @@ import React from 'react'
 import Layout from 'layouts/default'
 import CompleteSuggestion, { CompleteSuggestionData } from 'templates/cody-suggestion/complete-suggestion'
 import CompleteSuggestionAppBar from 'components/app-bar/complete-suggestion'
-import Navigation from 'components/navigation'
 import { GetServerSideProps } from 'next'
 import { ACCESS_TOKEN } from 'providers/auth'
 import { communicateWithContext } from 'lib/api'
 import parseJwt from 'lib/util/jwt'
+import CodySuggestionProvider from 'providers/cody-suggestion'
 
 export default function Page({ data } : { data : CompleteSuggestionData }) {
   return (
-    <Layout
-      header={<CompleteSuggestionAppBar />}
-      bottom={<Navigation />}
-    >
-      <CompleteSuggestion data={data} />
-    </Layout>
+    <CodySuggestionProvider>
+      <Layout
+        header={<CompleteSuggestionAppBar />}
+      >
+        <CompleteSuggestion data={data} />
+      </Layout>
+    </CodySuggestionProvider>
   )
 }
 
@@ -44,7 +45,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
   const { id } = context.params
 
-  const res = await communicateWithContext({ url: `/coord?coordId=${id}`, context })
+  const res = await communicateWithContext({ url: `/coord/${id}`, context })
 
   if (res.status !== 200) {
     throw new Error()
