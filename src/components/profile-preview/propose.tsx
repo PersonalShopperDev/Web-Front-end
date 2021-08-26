5c5c852b21d93a108b97b2f0046f27915e027ee9
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-08-27 01:33:28 +0900

    hotfix: Fix chat button on profile preview

--- src/components/profile-preview/propose.tsx
+++ src/components/profile-preview/propose.tsx
@@ -1,16 +1,39 @@
-import Link from 'next/link'
+import communicate from 'lib/api'
+import ERROR_MESSAGE from 'lib/constants/error'
+import { useRouter } from 'next/router'
+import { useAlert } from 'providers/dialog/alert/inner'
 import styles from 'sass/components/profile-preview/propose.module.scss'
 
 export default function Propose({ id }: { id: string }) {
-  const href = `/propose/new?uid=${id}`
+  const { createAlert } = useAlert()
+  const router = useRouter()
+
+  const onClick = async () => {
+    const res = await communicate({
+      url: '/chat',
+      payload: {
+        targetId: id,
+      },
+      method: 'POST',
+    })
+
+    if (res.status !== 200) {
+      await createAlert({ text: ERROR_MESSAGE })
+      return
+    }
+
+    const { roomId } = await res.json()
+
+    router.push(`/chat/${roomId}`)
+  }
 
   return (
     <section className={styles.container}>
-      <Link href={href}>
-        <a href={href} className={styles.button}>
-          견적 보내기
-        </a>
-      </Link>
+      <div className={styles.buttonWrapper}>
+        <button className={styles.button} type="button" onClick={onClick}>
+          대화하기
+        </button>
+      </div>
     </section>
   )
 }
