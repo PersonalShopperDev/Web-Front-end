0f378383f3a2e914b3b5614e1339ab22eff687f2
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-09-06 02:22:41 +0900

    fix: onboarding providers changes

--- src/providers/onboarding/index.tsx
+++ src/providers/onboarding/index.tsx
@@ -3,45 +3,60 @@ import React, {
   createContext, useContext, useEffect, useState, Dispatch, SetStateAction, useRef,
 } from 'react'
 
+type OnEditCallback = () => Promise<void>
+
+interface Skin {
+  id: number,
+  value: string
+}
+
+interface ClothSize {
+  topSize?: number
+  bottomSize?: number
+  shoulderSize?: number
+  waistSize?: number
+  bellySize?: number
+  hipSize?: number
+}
+
+interface ClothPrice {
+  topPrice?: {
+    min: number
+    max: number
+  }
+  bottomPrice?: {
+    min: number
+    max: number
+  }
+  dressPrice?: {
+    min: number
+    max: number
+  }
+  shoesPrice?: {
+    min: number
+    max: number
+  }
+  bagPrice?: {
+    min: number
+    max: number
+  }
+  accessoryPrice?: {
+    min: number
+    max: number
+  }
+  hatPrice?: {
+    min: number
+    max: number
+  }
+}
+
 interface Information {
     userType: 'N' | 'D' | 'S' | 'W'
-    gender: 'M' | 'F'
+    gender: 'M' | 'F' | ''
     body?: number
-    skin?: number
-    topSize?: number
-    bottomSize?: number
-    shoulderSize?: number
-    waistSize?: number
-    bellySize?: number
-    hipSize?: number
-    topPrice?: {
-      min: number
-      max: number
-    }
-    bottomPrice?: {
-      min: number
-      max: number
-    }
-    dressPrice?: {
-      min: number
-      max: number
-    }
-    shoesPrice?: {
-      min: number
-      max: number
-    }
-    bagPrice?: {
-      min: number
-      max: number
-    }
-    accessoryPrice?: {
-      min: number
-      max: number
-    }
-    hatPrice?: {
-      min: number
-      max: number
-    }
+    skin?: Skin
+    clothSize?: ClothSize
+    clothPrice? : ClothPrice
     styles?: Array<any>
     supplyMale?: boolean
     supplyFemale?: boolean
@@ -53,9 +68,11 @@ interface OnboardingProps {
     stylePicture: Array<any>
     setData: (key: string, value: string | number | boolean,
         min?: boolean, max?: boolean) => void
-    setEdit: (key: string) => void
     setStylePicture: Dispatch<SetStateAction<any>>
     putOnboardingInfo: () => Promise<void>
+    setOnEdit: (callback: OnEditCallback) => void
+    onClickEdit: () => void
+    fetchInformationData: () => Promise<void>
 }
 
 const OnboardingContext = createContext<OnboardingProps>(null)
@@ -67,90 +84,89 @@ export default function OnboardingProvider({
 }: {
   children: React.ReactNode
 }) {
-  const [information, setInformation] = useState<Information>(null)
+  const [information, setInformation] = useState<Information>({
+    userType: 'N',
+    gender: '',
+    clothSize: {},
+    clothPrice: {},
+  })
   const [stylePicture, setStylePicture] = useState([])
   const informationRef = useRef<any>()
+  const onEdit = useRef<OnEditCallback>()
+
+  const setOnEdit = (callback: OnEditCallback) => {
+    onEdit.current = callback
+  }
+
+  const onClickEdit = () => {
+    onEdit.current?.call(null)
+  }
 
   const setData = (key: string, value: string | number | boolean,
     min?: boolean, max?: boolean) => {
-    if (informationRef.current[key] === undefined && informationRef.current.userType === 'D') {
-      if (informationRef.current.gender === 'M') {
-        informationRef.current.topPrice = { max: 100000, min: 5000 }
-        informationRef.current.bottomPrice = { max: 70000, min: 5000 }
-        informationRef.current.shoesPrice = { max: 100000, min: 10000 }
-        informationRef.current.bagPrice = { max: 100000, min: 10000 }
-        informationRef.current.hatPrice = { max: 50000, min: 5000 }
+    if (key === 'gender' && information.userType === 'D') {
+      if (value === 'M') {
+        setInformation((prevInfo) => ({
+          ...prevInfo,
+          clothPrice: {
+            topPrice: { max: 100000, min: 5000 },
+            bottomPrice: { max: 70000, min: 5000 },
+            shoesPrice: { max: 100000, min: 10000 },
+            bagPrice: { max: 100000, min: 10000 },
+            hatPrice: { max: 50000, min: 5000 },
+          },
+        }))
       } else {
-        informationRef.current.topPrice = { max: 100000, min: 5000 }
-        informationRef.current.bottomPrice = { max: 70000, min: 5000 }
-        informationRef.current.dressPrice = { max: 100000, min: 5000 }
-        informationRef.current.shoesPrice = { max: 100000, min: 10000 }
-        informationRef.current.bagPrice = { max: 100000, min: 10000 }
-        informationRef.current.accessoryPrice = { max: 50000, min: 5000 }
+        setInformation((prevInfo) => ({
+          ...prevInfo,
+          clothPrice: {
+            topPrice: { max: 100000, min: 5000 },
+            bottomPrice: { max: 70000, min: 5000 },
+            dressPrice: { max: 100000, min: 5000 },
+            shoesPrice: { max: 100000, min: 10000 },
+            bagPrice: { max: 100000, min: 10000 },
+            accessoryPrice: { max: 50000, min: 5000 },
+          },
+        }))
       }
     }
     if (max) {
-      setInformation((prevInfo) => ({ ...prevInfo, [key]: { ...prevInfo[key], max: value } }))
-      informationRef.current[key].max = value
+      setInformation((prevInfo) => ({
+        ...prevInfo,
+        clothPrice: { ...prevInfo.clothPrice, max: value },
+      }))
     } else if (min) {
-      setInformation((prevInfo) => ({ ...prevInfo, [key]: { ...prevInfo[key], min: value } }))
-      informationRef.current[key].min = value
+      setInformation((prevInfo) => ({
+        ...prevInfo,
+        clothPrice: { ...prevInfo.clothPrice, min: value },
+      }))
+    } else if (key.substr(key.length - 4) === 'Size') {
+      setInformation((prevInfo) => ({
+        ...prevInfo,
+        clothSize: { ...prevInfo.clothSize, [key]: value },
+      }))
     } else {
       setInformation((prevInfo) => ({ ...prevInfo, [key]: value }))
-      informationRef.current[key] = value
     }
   }
-  const setEdit = async (key:string) => {
-    const payload: any = {}
-    if (key === 'body' || key === 'skin' || key === 'career') {
-      payload[key] = informationRef.current[key]
-    } else if (key === 'size') {
-      payload.topSize = informationRef.current.topSize
-      payload.bottomSize = informationRef.current.bottomSize
-      payload.shoulderSize = informationRef.current.shoulderSize
-      payload.waistSize = informationRef.current.waistSize
-      payload.bellySize = informationRef.current.bellySize
-      payload.hipSize = informationRef.current.hipSize
-    } else if (key === 'price') {
-      payload.topPrice = informationRef.current.topPrice
-      payload.bottomPrice = informationRef.current.bottomPrice
-      payload.shoesPrice = informationRef.current.shoesPrice
-      payload.bagPrice = informationRef.current.bagPrice
-      if (information.accessoryPrice !== undefined) {
-        payload.accessoryPrice = informationRef.current.accessoryPrice
-      }
-      if (information.dressPrice !== undefined) {
-        payload.dressPrice = informationRef.current.dressPrice
-      }
-      if (information.hatPrice !== undefined) payload.hatPrice = informationRef.current.hatPrice
-    } else if (key === 'codyGender') {
-      payload.supplyMale = informationRef.current.supplyMale
-      payload.supplyFemale = informationRef.current.supplyFemale
-    } else {
-      payload.list = stylePicture
-      await communicate({ url: '/style/img', payload, method: 'PUT' })
-      fetchInformationData()
-      return
-    }
-    communicate({ url: '/onboard', payload, method: 'PATCH' })
-  }
+
   const fetchInformationData = async () : Promise<void> => {
     const res = await communicate({
-      url: '/onboard',
+      url: '/profile',
     })
     if (res.status === 200) {
       const data = await res.json()
       setInformation(data)
     }
   }
+
   const putOnboardingInfo = async () => {
-    const payload = { list: stylePicture }
     await communicate({ url: '/onboard', payload: information, method: 'PUT' }).then(async (res) => {
       if (!res.ok) {
         throw new Error()
       }
       if (information.userType === 'D') {
-        await communicate({ url: '/style/img', payload, method: 'PUT' })
+        await communicate({ url: '/style/img', payload: { list: stylePicture }, method: 'PUT' })
       }
     })
   }
@@ -158,17 +174,20 @@ export default function OnboardingProvider({
   useEffect(() => {
     fetchInformationData()
   }, [])
+
   useEffect(() => {
     informationRef.current = information
-    if (informationRef.current === null) informationRef.current = { userType: 'N' }
   }, [information])
+
   const value = {
     information,
     stylePicture,
     setData,
-    setEdit,
     setStylePicture,
     putOnboardingInfo,
+    setOnEdit,
+    onClickEdit,
+    fetchInformationData,
   }
 
   return <OnboardingContext.Provider value={value}>{children}</OnboardingContext.Provider>
