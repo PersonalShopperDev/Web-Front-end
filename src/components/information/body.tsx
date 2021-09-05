fe6882d29181cd520a823359025308ad90474be5
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-09-06 02:21:34 +0900

    fix: refactor information components with editing

--- src/components/information/body.tsx
+++ src/components/information/body.tsx
@@ -1,25 +1,41 @@
 /* eslint-disable no-nested-ternary */
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import styles from 'sass/components/body.module.scss'
 import { useOnboarding } from 'providers/onboarding'
 import communicate from 'lib/api'
 
 export default function Body({
   isOnboarding,
+  isEdit,
 }: {
   isOnboarding?: boolean
+  isEdit?: boolean
 }) {
-  const { information, setData, setEdit } = useOnboarding()
+  const {
+    information, setData, setOnEdit, fetchInformationData,
+  } = useOnboarding()
   const [bodyType, setBodyType] = useState(null)
+  const [bodyId, setBodyId] = useState(0)
+  const bodyRef = useRef()
+
+  const onEditBody = async () => {
+    await communicate({ url: '/profile', payload: { body: bodyRef.current }, method: 'PATCH' })
+    fetchInformationData()
+  }
 
   const onBodyClick = (id) => {
-    setData('body', id)
-    if (!isOnboarding) setEdit('body')
+    setBodyId(id)
+    if (isOnboarding) {
+      setData('body', id)
+    } else {
+      bodyRef.current = id
+    }
   }
+
   useEffect(() => {
     const fetchBodyData = async () : Promise<void> => {
       const res = await communicate({
-        url: `/onboard/body?gender=${information.gender}`,
+        url: `/profile/body?gender=${information.gender}`,
       })
       if (res.status === 200) {
         const data = await res.json()
@@ -27,36 +43,55 @@ export default function Body({
       }
     }
     fetchBodyData()
+    if (information.body) {
+      setBodyId(information.body.id)
+    }
   }, [])
+
+  useEffect(() => {
+    if (isEdit) setOnEdit(onEditBody)
+  }, [isEdit])
+
   return (
-    <div className={isOnboarding ? styles.container : styles.infoContainer}>
-      {bodyType !== null && bodyType.map((item, index) => (
-        <div key={item.id}>
-          <button
-            type="button"
-            className={isOnboarding ? (information.body === item.id
-              ? styles.selectedBodyForm
-              : styles.notSelectedBodyForm) : (information.body === item.id
-              ? styles.infoSelectedBodyForm : styles.infoNotSelectedBodyForm)}
-            onClick={() => onBodyClick(item.id)}
-          >
-            <div>
-              <img src={item.img} alt="bodyType" />
-            </div>
-            <span className={information.body === item.id
-              ? styles.selectedText : null}
-            >
-              { String.fromCharCode(index + 65)}
-              .
-              {item.value}
-            </span>
-          </button>
-        </div>
-      ))}
-    </div>
+    <>
+      {isEdit
+        ? (
+          <div className={isOnboarding ? styles.container : styles.infoContainer}>
+            {bodyType && bodyType.map((item, index) => (
+              <div key={item.id}>
+                <button
+                  type="button"
+                  className={isOnboarding ? (bodyId === item.id
+                    ? styles.selectedBodyForm
+                    : styles.notSelectedBodyForm) : (bodyId === item.id
+                    ? styles.infoSelectedBodyForm : styles.infoNotSelectedBodyForm)}
+                  onClick={() => onBodyClick(item.id)}
+                >
+                  <div>
+                    <img src={item.img} alt="bodyType" />
+                  </div>
+                  <span className={bodyId === item.id
+                    ? styles.selectedText : null}
+                  >
+                    { String.fromCharCode(index + 65)}
+                    .
+                    {item.value}
+                  </span>
+                </button>
+              </div>
+            ))}
+          </div>
+        )
+        : (
+          <span className={styles.bodyFormText}>
+            {bodyType && bodyType.find((body) => body.id === bodyId).value}
+          </span>
+        ) }
+    </>
   )
 }
 
 Body.defaultProps = {
   isOnboarding: false,
+  isEdit: true,
 }
--- src/components/information/style-text.tsx
+++ src/components/information/style-text.tsx
@@ -1,11 +1,24 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import communicate from 'lib/api'
 import { useOnboarding } from 'providers/onboarding'
 import styles from 'sass/components/style-text.module.scss'
 
-export default function StyleText() {
-  const { information } = useOnboarding()
+export default function StyleText({
+  isEdit,
+}: {
+  isEdit: boolean
+}) {
+  const { information, setOnEdit, fetchInformationData } = useOnboarding()
   const [styleLists, setStyleLists] = useState([])
+  const [isOverLength, setIsOverLength] = useState(false)
+  const [styleText, setStyleText] = useState(null)
+  const styleRef = useRef([])
+
+  const onEditStyle = async () => {
+    await communicate({ url: '/style', payload: { list: styleRef.current }, method: 'PUT' })
+    fetchInformationData()
+  }
+
   useEffect(() => {
     async function fetchData() {
       const res = await communicate({ url: '/style' })
@@ -14,17 +27,70 @@ export default function StyleText() {
     }
     fetchData()
   }, [])
+
+  useEffect(() => {
+    if (isEdit) setOnEdit(onEditStyle)
+  }, [isEdit])
+
+  useEffect(() => {
+    if (styleLists.length !== 0) {
+      setStyleText(information.styles.map(({ id }) => id))
+      styleRef.current = information.styles.map(({ id }) => id)
+    }
+  }, [styleLists])
+
+  const onClickStyle = (id: number) => {
+    if (styleText.includes(id)) {
+      if (isOverLength) {
+        setIsOverLength(false)
+      }
+      setStyleText((list) => list.filter((item) => item !== id))
+      styleRef.current = styleRef.current.filter((item) => item !== id)
+      return
+    }
+    if (styleText.length < 3) {
+      setStyleText((list) => [...list, id])
+      styleRef.current.push(id)
+      return
+    }
+    setIsOverLength(true)
+  }
+
   return (
     <div className={styles.container}>
-      {styleLists.map((value) => (
-        <div
-          className={information.styles.findIndex((e) => e.id === value.id) !== -1
-            ? styles.selectedBox : styles.notSelectedBox}
-          key={Math.random()}
-        >
-          {value.value}
-        </div>
-      ))}
+      {isEdit
+        ? (
+          <>
+            <span className={isOverLength ? styles.over : styles.limit}>최대 3개 선택</span>
+            <div>
+              {styleLists.map(({ value, id }) => (
+                <button
+                  type="button"
+                  className={styleText.includes(id)
+                    ? styles.selectedBox : styles.notSelectedBox}
+                  key={Math.random()}
+                  onClick={() => onClickStyle(id)}
+                >
+                  <span className={styles.styleText}>
+                    {value}
+                  </span>
+                </button>
+              ))}
+            </div>
+          </>
+        )
+        : (
+          <>
+            {information.styles.map(({ value }) => (
+              <div
+                key={Math.random()}
+                className={styles.notSelectedBox}
+              >
+                <span className={styles.styleText}>{value}</span>
+              </div>
+            ))}
+          </>
+        )}
     </div>
   )
 }
