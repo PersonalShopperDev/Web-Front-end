0fa840a3f58c7a1013f5d5b59015dc26bd18d3e7
Junseo Oh <insung9546@gmail.com>
2021-10-17 20:31:53 +0900

    fix: Wrap link with A tag

--- src/components/stylist-box.tsx
+++ src/components/stylist-box.tsx
@@ -23,42 +23,44 @@ export default function StylistBox({
 }) {
   return (
     <Link href={`/profile/${info.id}`}>
-      <div className={styles.listBox}>
-        <img src={info.img} alt={info.name} className={styles.profileImg} />
-        <div className={styles.infoBox}>
-          <div>
-            <span className={styles.name}>{info.name}</span>
-            <span className={styles.stylistText}>스타일리스트</span>
-          </div>
-          {info.price
-          && (
-          <span className={styles.priceText}>
-            {info.price}
-            원
-          </span>
-          ) }
-          {info.rating && info.reviewCount && info.hireCount
-          && (
-          <span className={styles.indicatorText}>
-            {info.rating}
-            점
-            <span>|</span>
-            {info.reviewCount}
-            리뷰
-            <span>|</span>
-            {info.hireCount}
-            고용
-          </span>
-          ) }
-          <div>
-            {info.styleType.map((item) => (
-              <span className={styles.style} key={item.id}>
-                {item.value}
-              </span>
-            ))}
+      <a href={`/profile/${info.id}`}>
+        <div className={styles.listBox}>
+          <img src={info.img} alt={info.name} className={styles.profileImg} />
+          <div className={styles.infoBox}>
+            <div>
+              <span className={styles.name}>{info.name}</span>
+              <span className={styles.stylistText}>스타일리스트</span>
+            </div>
+            {info.price
+            && (
+            <span className={styles.priceText}>
+              {info.price}
+              원
+            </span>
+            ) }
+            {info.rating && info.reviewCount && info.hireCount
+            && (
+            <span className={styles.indicatorText}>
+              {info.rating}
+              점
+              <span>|</span>
+              {info.reviewCount}
+              리뷰
+              <span>|</span>
+              {info.hireCount}
+              고용
+            </span>
+            ) }
+            <div>
+              {info.styleType.map((item) => (
+                <span className={styles.style} key={item.id}>
+                  {item.value}
+                </span>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      </a>
     </Link>
   )
 }
